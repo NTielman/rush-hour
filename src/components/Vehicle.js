3f9f95ad6d06494614ba587bebb5ae64ee2c94d4
Nneka Tielman <khalie_nne@yahoo.com>
2022-03-23 21:03:57 +0100

    added drag listeners to vehicle

--- src/components/Vehicle.js
+++ src/components/Vehicle.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Vehicle({ orientation = "horizontal" }) {
 
+    const [invisibility, setInvisibility] = useState(false)
+
+    // Make vehicle invisble on drag
+    const handleDragStart = () => {
+        setTimeout(() => setInvisibility(true));
+    }
+
+    // Make vehicle visible again on release
+    const handleDragEnd = () => {
+        setInvisibility(false);
+    }
+
     return (
-        <div className={`vehicle ${orientation}`}>
+        <div
+            className={`vehicle ${orientation} ${invisibility && 'hide'}`}
+            draggable="true"
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}>
             <span className="vehicle-body">
-                <span className="vehicle-top">
-                </span>
+                <span className="vehicle-top"></span>
             </span>
         </div>
     )
